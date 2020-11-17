0c933f7d2b5ff0908e9447f768d50c01f8bde1cc
Akram_Alam <arundelfootball23@gmail.com>
2020-11-17 18:11:01 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,11 +9,9 @@ class School
          @name = name 
         @roster = {}
     end 
-
     def add_student(student, grade)
         roster[grade] ||= []
         roster[grade] << student
-        
         
     end 
 
